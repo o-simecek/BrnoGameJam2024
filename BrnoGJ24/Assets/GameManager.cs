83905d8b44d9f0d65939af3144fdcd81ddd176ec
o-simecek <84139504+o-simecek@users.noreply.github.com>
2024-04-05 22:19:29 +0200

    starting, game manager

---
+++ BrnoGJ24/Assets/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+
+    public static GameManager Instance { get { 
+            return _instance; } }
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
